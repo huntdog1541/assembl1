a1ea6a46eb46904c89dfe72e373dd482381c9883
David Hunt <huntdog1541@gmail.com>
2014-10-07 11:39:03 -0500

    added to the javascript

--- assembl1.js
+++ assembl1.js
@@ -6,31 +6,39 @@ function submit() {
 }
 
 function parse(text) {
-    text.split(" ");
-    text.split(",");
-    var command = text[0];
-    console.log("The command is " + command + " from " + text);
-    var n3 = 3;
-    var n4 = 4;
-    addfunct(n3, n4);
+    var arry = text.split(" ");
+    var command = arry[0];
+    console.log("The command is " + command);
+	if(command == "ADD")
+		addCommand(arry);
 }
 
 function addfunct(reg1, reg2)
 {
-    var num1, num2;
-    console.log(reg1);
-    console.log(reg2);
-    if(document.getElementById(reg1) == null)
-        num1 = 0;
-    else
-        num1 = document.getElementById(reg1).value;
-    
-    if(document.getElementById(reg2) == null)
-        num2 = 0;
-    else
-        num2 = document.getElementById(reg2).value;
-    
-    num1 = num1 + num2;
-    console.log(num1);
-    document.getElementById(reg1).value = num1;
+	reg1 = reg1 + reg2;
+    document.getElementById('reg1').value= reg1;
+}
+
+function addCommand(arry)
+{
+	var n1, n2, n3;
+	if(arry.length == 3)
+	{
+		console.log("The length is 3");
+		arry[1].replace(',', '');
+		console.log(arry[1]);
+		switch(arry[1])
+		{
+			case "reg1": n1 = document.getElementById('reg1').value; break;
+			case "reg2": n2 = document.getElementById('reg2').value; break;
+			case "reg3": n3 = document.getElementById('reg3').value; break;
+		}
+		switch(arry[2])
+		{
+			case "reg1": n1 = document.getElementById('reg1').value; break;
+			case "reg2": n2 = document.getElementById('reg2').value; break;
+			case "reg3": n3 = document.getElementById('reg3').value; break;
+		}
+		addfunct(n1, n2);
+	}
 }
